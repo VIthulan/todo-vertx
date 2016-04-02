5d7fd488f0426ac31b8583fa629214d1ed3179cb
VIthulan <mvvithulan@gmail.com>
2016-04-02 16:16:04 +0530

    correlating to url param

--- src/main/java/io/vertx/util/DBclient.java
+++ src/main/java/io/vertx/util/DBclient.java
@@ -59,12 +59,14 @@ public class DBclient {
         JsonObject taskJson = new JsonObject()
                 .put("title", task.getTitle())
                 .put("completed",task.getCompleted())
-                .put("order",task.getOrder());
+                .put("order",task.getOrder())
+                .put("url",task.getUrl());
 
         mongoClient.insert(COLLECTION_NAME, taskJson, res -> {
             if (res.succeeded()) {
                 log.info("Successfully inserted: " + res.result());
                 JsonObject jsonWithURL = new JsonObject()
+                        .put("_id",res.result())
                         .put("title", task.getTitle())
                         .put("completed",task.getCompleted())
                         .put("order",task.getOrder())
